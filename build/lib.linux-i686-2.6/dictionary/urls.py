f91bf3c80a24b8a2712d920b8231ced4656ee6c7
Albert Pi <albertpi@gmail.com>
2012-04-23 20:46:20 +0200

    Cleanup

--- build/lib.linux-i686-2.6/dictionary/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from dictionary import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.list, name='dictionary_list'),
-   (r'^(?P<slug>\w+)/$', 'list_detail.object_detail', {'template': 'dictionary_detail.html'}),
-)
-
